611ee378688a14bb0a447537fdb145e27b5f453d
Ajit Behera <ajitbehera@Ajits-MacBook-Air.local>
2019-03-10 07:08:56 -0400

    "removed security startup"

--- src/main/java/com/sample/springbootms/controller/WelcomeController.java
+++ src/main/java/com/sample/springbootms/controller/WelcomeController.java
@@ -1,17 +1,24 @@
 package com.sample.springbootms.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sample.springbootms.feignservice.ApplicantFeignService;
 
 @RestController
 public class WelcomeController {
 
 	@Value("${welcome.message}")
 	private String welcomeMessage;
+	
+	@Autowired
+	ApplicantFeignService feignService;
 
 	@GetMapping("/ms/msg")
 	public String retrieveWelcomeMessage() {
-		return welcomeMessage;
+    	return feignService.getApplicants();
+		//return welcomeMessage;
 	}
 }
--- src/main/java/com/sample/springbootms/feignservice/ApplicantFeignService.java
+++ src/main/java/com/sample/springbootms/feignservice/ApplicantFeignService.java
@@ -4,7 +4,8 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 //@FeignClient(name="applicant-service" )//Service Id of applicant service
-@FeignClient(name="applicant-service",url="http://localhost:8080")
+//,url="http://localhost:8081"
+@FeignClient(name="applicant-service")
 public interface ApplicantFeignService {
 	
    @RequestMapping("/getApplicants")
